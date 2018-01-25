716a668aa34807b3eef33c60888d46a5d891684f
Wes Etheredge <jwesleye@gmail.com>
2018-01-25 16:47:07 -0600

    inital data generator args

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,12 @@
-from random import seed, randint
-
+try:
+    from random import seed, randint
+    import json
+    import argparse
+    import sys
+    from time import sleep
+    print("Imports are good")
+except Exception as e:
+    print("IMPORT ERROR", e)
 
 # set for consistent randomness
 seed(42)
@@ -7,3 +14,9 @@ seed(42)
 #     print(randint(0, 99))
 #     # should print 63, 2, 27
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--numberOfElements', help='Number of Data Elements to create', type=int)
+parser.add_argument('--outputFormat', help='Select from JSON, CSV, SQL, TSV, AVRO', type=str)
+# parser.add_argument('--foo', help='Foo the program')
+args = parser.parse_args()
+print(args)
